697c7803cccdc7b19a20ab00870ca1e08dc2bcb9
Lion Kortlepel <development@kortlepel.com>
2021-09-08 15:03:39 +0300

    fix operator= behavior, move operator+ and operator- out of the class

--- include/RawPtr.h
+++ include/RawPtr.h
@@ -9,27 +9,34 @@ struct RawPtr final {
     RawPtr() = default;
     explicit RawPtr(T* value)
         : ptr(value) { }
-    RawPtr(const RawPtr&) = delete;
-    RawPtr& operator=(const RawPtr&) = delete;
-    RawPtr& operator=(T* p) {
+    RawPtr& operator=(const T* p) {
         ptr = p;
         return *this;
     }
     RawPtr(RawPtr&& other)
         : ptr(other.ptr) { other.ptr = nullptr; }
-    RawPtr& operator=(RawPtr& other) { swap(other); }
+    RawPtr& operator=(RawPtr& other) {
+        swap(other);
+        return *this;
+    }
+    RawPtr& operator=(RawPtr&& other) {
+        ptr = other.ptr;
+        other.ptr = nullptr;
+        return *this;
+    }
     void swap(RawPtr& other) { std::swap(ptr, other.ptr); }
     T& operator*() { return *ptr; }
     const T& operator*() const { return *ptr; }
     T* operator->() { return ptr; }
     const T* operator->() const { return ptr; }
-    operator bool() const { return bool(ptr); }
-    // only support very basic pointer arithmetic to avoid smelly code
-    RawPtr operator+(std::size_t offset) const { return RawPtr(ptr + offset); }
-    RawPtr operator-(std::size_t offset) const { return RawPtr(ptr - offset); }
     T* ptr { nullptr };
 };
 
+// only support very basic pointer arithmetic to avoid smelly code
+template<class T>
+RawPtr<T> operator+(const RawPtr<T>& ptr, std::size_t offset) { return RawPtr<T>(ptr.ptr + offset); }
+template<class T>
+RawPtr<T> operator-(const RawPtr<T>& ptr, std::size_t offset) { return RawPtr<T>(ptr.ptr - offset); }
 // comparison operators
 template<class T>
 bool operator==(const RawPtr<T>& a, const RawPtr<T>& b) { return a.ptr == b.ptr; }
@@ -51,12 +58,3 @@ struct hash<RawPtr<T>> {
     size_t operator()(const RawPtr<T>& ptr) { return std::hash<T*>(ptr.ptr); }
 };
 };
-
-void n() {
-    int x;
-    unsigned y;
-    RawPtr<int> a;
-    a = &x;
-    RawPtr<unsigned> b;
-    b = std::move(a);
-}
